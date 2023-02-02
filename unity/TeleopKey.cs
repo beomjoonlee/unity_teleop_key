63693f54b987c1a6ca9016928a5b628ed65f8bab
beomjoonlee <beomjoon.b.lee@gmail.com>
2023-02-02 22:59:30 +0900

    TeleopKey.cs

--- unity/TeleopKey.cs
+++ unity/TeleopKey.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,15 +18,17 @@ public class TeleopKey : MonoBehaviour
 
     void Move(Twist velocityMessage)
     {
-        float vx = (float)-velocityMessage.linear.y;
-        float vy = (float)-velocityMessage.linear.z;
-        float vz = (float)velocityMessage.linear.x;
         float rx = (float)-velocityMessage.angular.y;
         float ry = (float)-velocityMessage.angular.z;
         float rz = (float)velocityMessage.angular.x;
 
         Vector3 turn = new Vector3(rx, ry, rz);
         robotRigidbody.rotation = robotRigidbody.rotation * Quaternion.Euler(turn);
+        float euler_angle = robotRigidbody.rotation.eulerAngles[1];
+
+        float vx = (float)velocityMessage.linear.x * (float)Math.Cos(Math.PI/2 - (euler_angle * Math.PI / 180)) - (float)velocityMessage.linear.y * (float)Math.Sin(Math.PI/2 - (euler_angle * Math.PI / 180));
+        float vy = (float)velocityMessage.linear.z;
+        float vz = (float)velocityMessage.linear.x * (float)Math.Sin(Math.PI/2 - (euler_angle * Math.PI / 180)) + (float)velocityMessage.linear.y * (float)Math.Cos(Math.PI/2 - (euler_angle * Math.PI / 180));
 
         Vector3 moveDistance = new Vector3(vx, vy, vz);
         robotRigidbody.MovePosition(robotRigidbody.position + moveDistance);
